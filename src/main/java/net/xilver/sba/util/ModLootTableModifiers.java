77793452df22b91507f67e3681edbc9d328a4a4b
Xilverr <chloe.clausen22@gmail.com>
2023-08-29 21:30:06 -0700

    -Bamboo Trade for Wandering Traders -End Portal Frame & Ancient Debris Drops from Wither -Netherrack Added to Piglin Bartering pool (unfinished)

--- src/main/java/net/xilver/sba/util/ModLootTableModifiers.java
+++ src/main/java/net/xilver/sba/util/ModLootTableModifiers.java
@@ -1,35 +1,53 @@
 package net.xilver.sba.util;
 
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
+import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.boss.WitherEntity;
+import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.collection.Weight;
 
 public class ModLootTableModifiers {
     private static final Identifier WITHER_ID
             = new Identifier("minecraft","entities/wither");
+    private static final Identifier BARTER
+            = new Identifier("minecraft", "gameplay/piglin_bartering");
+
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-
+            //checks for Wither Loot Table
             if(WITHER_ID.equals(id)) {
-
-                LootPool.Builder poolBuilder = LootPool.builder()
+                //Adds Ancient Debris and End Portal Frames to the loot table
+                LootPool.Builder ancientdebris = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .conditionally(RandomChanceLootCondition.builder(0.5f))
                         .with(ItemEntry.builder(Blocks.ANCIENT_DEBRIS))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0f, 4.0f)).build())
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0f, 4.0f)).build());
+                tableBuilder.pool(ancientdebris.build());
+                LootPool.Builder endportalframe = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(0.5f))
                         .with(ItemEntry.builder(Blocks.END_PORTAL_FRAME))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 1.0f)).build());
-                tableBuilder.pool(poolBuilder.build());
-
+                tableBuilder.pool(endportalframe.build());
+            }
+            if(BARTER.equals(id)) {
+                //Adds Netherrack to Bartering Trades
+                LootPool.Builder netherrack = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .with(ItemEntry.builder(Blocks.NETHERRACK))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0f, 8.0f)).build());
+                tableBuilder.pool(netherrack.build());
             }
 
         });
